720f1ee26ac9d29ffac5048d399771f7cae3c4b3
Hervé Duong <duong.herve@gmail.com>
2024-06-03 00:00:58 +0200

    fix : tâches

--- front/src/components/Parametres.vue
+++ front/src/components/Parametres.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="widget flex fixed z-10 top-0 right-0 mt-24 mr-12 rounded bg-neutral-900 p-4 w-full text-white flex-col gap-4">
+  <div class="widget flex fixed z-10 top-0 right-0 mt-24 mr-12 rounded bg-neutral-900 p-4 w-full text-white flex-col border-neutral-800 border gap-4">
     <span>Hervé Duong</span>
     <hr class="border border-zinc-800">
     <span>mon.email@gmail.com</span>
--- front/src/views/Accueil.vue
+++ front/src/views/Accueil.vue
@@ -1,15 +1,43 @@
 <template>
   <Parametres v-if="showParametres"></Parametres>
-  <div class="flex w-full p-6 flex-col h-dvh bg-black gap-2 relative">
+  <div class="flex w-full p-6 flex-col h-dvh bg-black gap-2 relative items-center">
     <nav class="flex w-full p-4 px-6 justify-between items-center">
       <h1 class="text-white">Plume</h1>
       <h1 class="profil bg-emerald-500 text-white p-2 rounded-full cursor-pointer" @click="toggleParametres">HD</h1>
     </nav>
     <hr class="w-full flex border-zinc-800">
-    <div class="tasks flex w-full flex-col gap-2 mt-2">
-      <small class="text-white flex w-full items-center justify-center">Vous n'avez pas de tâche</small>
+    <div class="tasks flex w-full flex-col gap-2 mt-2 max-w-screen-lg overflow-y-auto overflow-x-hidden">
+<!--      <small class="text-white flex w-full items-center justify-center">Vous n'avez pas de tâche</small>-->
+      <small class="text-white font-black">Sport</small>
+      <label for="check" class="task flex w-full bg-neutral-900 px-6 p-2 rounded items-center gap-4 text-white min-h-16">
+        <input type="checkbox" name="check" id="check" class="flex w-5 h-5">
+        <span>Faire 20 pompes</span>
+      </label>
+      <label for="check" class="task flex w-full bg-neutral-900 px-6 p-2 rounded items-center gap-4 text-white min-h-16">
+        <input type="checkbox" name="check" id="check" class="flex w-5 h-5">
+        <span>50 squats</span>
+      </label>
+      <hr class="border border-neutral-900 flex w-full m-6">
+      <small class="text-white font-black">Chambre</small>
+      <label for="check" class="task flex w-full bg-neutral-900 px-6 p-2 rounded items-center gap-4 text-white min-h-16">
+        <input type="checkbox" name="check" id="check" class="flex w-5 h-5">
+        <span>Faire le lit</span>
+      </label>
+      <label for="check" class="task flex w-full bg-neutral-900 px-6 p-2 rounded items-center gap-4 text-white min-h-16">
+        <input type="checkbox" name="check" id="check" class="flex w-5 h-5">
+        <span>Passer l'aspirateur</span>
+      </label>
+      <hr class="border border-neutral-900 flex w-full m-6">
+      <label for="check" class="task flex w-full bg-neutral-900 px-6 p-2 rounded items-center gap-4 text-white min-h-16">
+        <input type="checkbox" name="check" id="check" class="flex w-5 h-5">
+        <span>Déposer la lettre à la poste</span>
+      </label>
+      <label for="check" class="task flex w-full bg-neutral-900 px-6 p-2 rounded items-center gap-4 text-white min-h-16">
+        <input type="checkbox" name="check" id="check" class="flex w-5 h-5">
+        <span>Faire les courses</span>
+      </label>
     </div>
-    <div class="btn flex gap-8 items-center justify-center fixed bottom-0 w-full mb-12 left-0">
+    <div class="btn flex gap-8 items-center justify-center fixed bottom-0 w-full mb-12 left-0 ">
       <button class="text-white p-4 bg-emerald-500 rounded items-center justify-center w-full">Créer</button>
       <button class="text-white p-4 bg-red-500 rounded items-center justify-center w-full">Supprimer</button>
     </div>
