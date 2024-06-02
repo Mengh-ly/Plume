07dca2b5b2cd840daf43bc21fdc5dee92428a137
Hervé Duong <duong.herve@gmail.com>
2024-06-02 23:30:04 +0200

    fix : accueil, connexion, inscription

---
+++ front/src/components/Parametres.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="widget flex fixed z-10 top-0 right-0 mt-24 mr-12 rounded bg-neutral-900 p-4 w-full text-white flex-col gap-4">
+    <span>Hervé Duong</span>
+    <hr class="border border-zinc-800">
+    <span>mon.email@gmail.com</span>
+    <a href="" class="flex w-full p-2 bg-red-500 rounded items-center justify-center">Déconnexion</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Parametres',
+  }
+</script>
+
+<style scoped>
+.widget{
+  max-width: 300px;
+}
+</style>
---
+++ front/src/views/Accueil.vue
@@ -0,0 +1,45 @@
+<template>
+  <Parametres v-if="showParametres"></Parametres>
+  <div class="flex w-full p-6 flex-col h-dvh bg-black gap-2 relative">
+    <nav class="flex w-full p-4 px-6 justify-between items-center">
+      <h1 class="text-white">Plume</h1>
+      <h1 class="profil bg-emerald-500 text-white p-2 rounded-full cursor-pointer" @click="toggleParametres">HD</h1>
+    </nav>
+    <hr class="w-full flex border-zinc-800">
+    <div class="tasks flex w-full flex-col gap-2 mt-2">
+      <small class="text-white flex w-full items-center justify-center">Vous n'avez pas de tâche</small>
+    </div>
+    <div class="btn flex gap-8 items-center justify-center fixed bottom-0 w-full mb-12 left-0">
+      <button class="text-white p-4 bg-emerald-500 rounded items-center justify-center w-full">Créer</button>
+      <button class="text-white p-4 bg-red-500 rounded items-center justify-center w-full">Supprimer</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Test from '@/components/Parametres.vue';
+import Parametres from "@/components/Parametres.vue";
+
+export default {
+  name: 'connexion',
+  components: {
+    Parametres
+  },
+  data() {
+    return {
+      showParametres: false
+    }
+  },
+  methods: {
+    toggleParametres() {
+      this.showParametres = !this.showParametres;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn button{
+  max-width: 150px;
+}
+</style>
